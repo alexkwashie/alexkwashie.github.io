f54572cdfc8afc19a3c5eb0bfe633fc59d2080d3
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-10-09 16:01:16 +0100

    commit portfolio site

--- appo.js
+++ appo.js
@@ -3,7 +3,14 @@
 $(function() {
     $.scrollify({
       section : ".Page",
-
+      scrollbars: true,
     });
+    let windowWidth = $(window).width();
+
+    if (windowWidth < 750){
+      $.scrollify.destroy();
+    }
+
   });
+
 
